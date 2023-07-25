b06473002674183732576616e393439f67307e1a
emart126 <125086504+emart126@users.noreply.github.com>
2023-07-25 08:29:10 -0700

    added files for basic template

---
+++ player.h
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include "board.h"
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+// player is an entity class in the game that is able to manipulatae and play on a board
+class Player {
+    private:
+        // Player fields
+        std::string name;
+        std::string sign;
+
+    public:
+        // Class constructor -----------------------------------------------------------
+
+
+
+        // Access functions -------------------------------------------------------------
+
+
+
+        // Manipulation functions -------------------------------------------------------
+
+
+
+};
+
+#endif
