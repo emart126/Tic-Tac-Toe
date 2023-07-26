7df897914939d43310a1f819d8cfb99b4437c1c8
emart126 <125086504+emart126@users.noreply.github.com>
2023-07-26 07:50:00 -0700

    attempting to test player.cpp file in the main game file

--- game.cpp
+++ game.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 #include <string>
 #include "board.h"
+#include "player.h"
 
 using namespace std;
 
 int main() {
-    
+    Board myB = Board(4);
+    Player one = Player("Bob", "x");
+    Player two = Player("Alice", "o");
+    cout << "p1: " << one.getName() << ", " << one.getSign() << endl;
+    cout << "p2: " << two.getName() << ", " << two.getSign() << endl;
+    myB.show();
+    one.choose(myB);
+    myB.show();
+    two.choose(myB);
+    myB.show();
+    one.choose(myB);
+    myB.show();
+    two.choose(myB);
+    myB.show();
+    one.choose(myB);
+    myB.show();
+    two.choose(myB);
+    myB.show();
+    cout << "winner: " << myB.getWinner() << endl;
+
     return(0);
 }
