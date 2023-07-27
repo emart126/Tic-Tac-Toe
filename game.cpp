02384dbfb136f52326247f5d085b18c745cfb915
emart126 <125086504+emart126@users.noreply.github.com>
2023-07-27 08:20:09 -0700

    Implemented main function, game now runs without issues with basic tests

--- game.cpp
+++ game.cpp
@@ -6,25 +6,54 @@
 using namespace std;
 
 int main() {
-    Board myB = Board(4);
-    Player one = Player("Bob", "x");
-    Player two = Player("Alice", "o");
-    cout << "p1: " << one.getName() << ", " << one.getSign() << endl;
-    cout << "p2: " << two.getName() << ", " << two.getSign() << endl;
-    myB.show();
-    one.choose(&myB);
-    myB.show();
-    two.choose(&myB);
-    myB.show();
-    one.choose(&myB);
-    myB.show();
-    two.choose(&myB);
-    myB.show();
-    one.choose(&myB);
-    myB.show();
-    two.choose(&myB);
-    myB.show();
-    cout << "winner: " << myB.getWinner() << endl;
+    std::string name;
+    std::string winnerSign;
+    std::string again;
+    bool p1Turn = true;
+
+    cout << "Welcome to TIC-Tac-Toe Game!" << endl;
+    cout << "Please enter a name to start: ";
+    cin >> name;
+    Player player1 = Player(name, "X");
+    Player player2 = Player("Bob", "O");
+    
+    while (true) {
+        Board B = Board();
+        while (true) {
+            B.show();
+            if (p1Turn) {
+                player1.choose(&B);
+                p1Turn = false;
+            }
+            else {
+                player2.choose(&B);
+                p1Turn = true;
+            }
+
+            if (B.isDone()) {
+                break;
+            }
+        }
+        B.show();
+
+        winnerSign = B.getWinner();
+        if (winnerSign == player1.getSign()) {
+            cout << player1.getName() << "is the winner!" << endl;
+        }
+        else if (winnerSign == player2.getSign()) {
+            cout << player2.getName() << "is the winner!" << endl;
+        }
+        else {
+            cout << "It is a tie!" << endl;
+        }
+
+        cout << "Would you like to play again? [Y/N]" << endl;
+        cin >> again;
+        again[0] = toupper(again[0]);
+        if (again != "Y") {
+            break;
+        }
+    }
 
     return(0);
 }
